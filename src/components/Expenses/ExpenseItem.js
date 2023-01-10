8c1cede7b1417d5dce5fda424f62f7eb64ef91b6
pjevic <milanpjevic@icloud.com>
2023-01-10 18:57:26 +0100

    React state & working with events

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,14 +3,12 @@ import ExpenseDate from "../Expenses/ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const { title, amount, date } = props;
-
   return (
     <Card className="expense-item">
-      <ExpenseDate date={date} />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </Card>
   );
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import ExpenseItem from "../Expenses/ExpenseItem";
 import "./Expenses.css";
@@ -5,8 +7,15 @@ import "./Expenses.css";
 const Expenses = (props) => {
   const { expenses } = props;
 
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (year) => {
+    setFilteredYear(year);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onYearChange={filterChangeHandler} />
       <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
       <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
       <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
